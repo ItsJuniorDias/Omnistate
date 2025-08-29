573c260571e23b387bcf0c99f6e0294e79701981
Alexandre <reabilitado97@gmail.com>
2025-08-29 17:26:15 -0300

    chore: refactor and improvement register end point

---
+++ server/routes/userRoute.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getUserDetails,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
+  updateProfile,
+  getAllUsers,
+  getSingleUser,
+  updateUserRole,
+  deleteUser,
+} = require("../controllers/userController");
+const {
+  isAuthenticatedUser,
+  authorizeRoles,
+} = require("../middlewares/user_actions/auth");
+
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary").v2;
+
+const multer = require("multer");
+
+const router = express.Router();
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "uploads", // pasta no Cloudinary
+    allowed_formats: ["jpg", "png", "jpeg", "gif"],
+  },
+});
+
+const parser = multer({ storage: storage });
+
+router.route("/register").post(parser.single("avatar"), registerUser);
+
+router.route("/login").post(loginUser);
+router.route("/logout").get(logoutUser);
+
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
+
+router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
+
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers);
+
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
+
+module.exports = router;
