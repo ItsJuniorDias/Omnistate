39c051b3925ef4ad6373e689c621cb656f624adf
Alexandre <reabilitado97@gmail.com>
2025-08-31 14:30:27 -0300

    chore: install nodemailer

--- front-end/src/components/pages/NotFound.jsx
+++ front-end/src/components/pages/NotFound.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import { useEffect } from 'react';
+import React from "react";
+import { useEffect } from "react";
 
 const NotFound = () => {
-
-    useEffect(() => {
-        window.scrollTo(0, 0)
-    })
-    return (
-        <React.Fragment>
-            <section className="not-found">
-                <h1 className='page-heading'>Page not found</h1>
-	    <p>Error 404: The page your're looking for does not exist!</p>
-            </section>
-        </React.Fragment>
-    )
-}
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
+  return (
+    <React.Fragment>
+      <section className="not-found mt-[40px]">
+        <h1 className="page-heading">Page not found</h1>
+        <p>Error 404: The page your're looking for does not exist!</p>
+      </section>
+    </React.Fragment>
+  );
+};
 
 export default NotFound;
--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,156 +1,175 @@
-const axios = require('axios')
-const asyncErrorHandler = require('../middlewares/helpers/asyncErrorHandler');
-const Order = require('../models/orderModel');
-const Product = require('../models/productModel');
-const ErrorHandler = require('../utils/errorHandler');
-const sendEmail = require('../utils/sendEmail');
+const axios = require("axios");
+const asyncErrorHandler = require("../middlewares/helpers/asyncErrorHandler");
+const Order = require("../models/orderModel");
+const User = require("../models/userModel");
+const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
+const sendEmail = require("../utils/sendEmail");
+const transporter = require("../config/nodemailer");
+const userModel = require("../models/userModel");
 
 // Create New Order
 exports.newOrder = asyncErrorHandler(async (req, res, next) => {
+  const { shippingInfo, orderItems, paymentInfo, totalPrice, useREPmail } =
+    req.body;
 
-    const {
-        shippingInfo,
-        orderItems,
-        paymentInfo,
-        totalPrice,
-    } = req.body;
+  const orderExist = await Order.findOne({ paymentInfo });
 
-    const orderExist = await Order.findOne({ paymentInfo });
+  if (orderExist) {
+    return next(new ErrorHandler("Order Already Placed", 400));
+  }
 
-    if (orderExist) {
-        return next(new ErrorHandler("Order Already Placed", 400));
-    }
+  const order = await Order.create({
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    totalPrice,
+    paidAt: Date.now(),
+    user: req.user._id,
+  });
 
-    const order = await Order.create({
-        shippingInfo,
-        orderItems,
-        paymentInfo,
-        totalPrice,
-        paidAt: Date.now(),
-        user: req.user._id,
-    });
+  const user = await User.findById(req.user.id);
 
-    await sendEmail({
-        email: req.useREPmail,
-        templateId: process.env.SENDGRID_ORDER_TEMPLATEID,
+  const mailOptions = {
+    from: "reabilitado97@gmail.com",
+    to: useREPmail,
+    subject: "Cryptocurrency Purchase Confirmation",
+    text: `Hello ${user.name},
+
+      Thank you for your purchase! We have received your request to pay using cryptocurrency.
+
+      Transaction Details:
+
+      Product/Service: ${order.orderItems[0].name}
+
+      Quantity: ${order.orderItems[0].quantity}
+
+      Total Amount:  ${order.orderItems[0].price}
+
+      Please confirm the transaction so we can process your order. Once the payment is verified, you will receive a confirmation of shipment or access to the purchased service.
+
+      If you have any questions, our support team is available at: itsjuniordias1997@gmail.com.
+
+      Thank you for your business!`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.json({
+        error,
+      });
+    } else {
+      console.log(info.response, "INFO RESPONSE");
+
+      res.json({
         data: {
-            name: req.user.name,
-            shippingInfo,
-            orderItems,
-            totalPrice,
-            oid: order._id,
-        }
-    });
-
-    res.status(201).json({
-        success: true,
-        order,
-    });
+          info: info.response,
+          order,
+        },
+      });
+    }
+  });
 });
 
 // Get Single Order Details
 exports.getSingleOrderDetails = asyncErrorHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
 
-    const order = await Order.findById(req.params.id).populate("user", "name email");
+  if (!order) {
+    return next(new ErrorHandler("Order Not Found", 404));
+  }
 
-    if (!order) {
-        return next(new ErrorHandler("Order Not Found", 404));
-    }
-
-    res.status(200).json({
-        success: true,
-        order,
-    });
+  res.status(200).json({
+    success: true,
+    order,
+  });
 });
-
 
 // Get Logged In User Orders
 exports.myOrders = asyncErrorHandler(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
 
-    const orders = await Order.find({ user: req.user._id });
+  if (!orders) {
+    return next(new ErrorHandler("Order Not Found", 404));
+  }
 
-    if (!orders) {
-        return next(new ErrorHandler("Order Not Found", 404));
-    }
-
-    res.status(200).json({
-        success: true,
-        orders,
-    });
+  res.status(200).json({
+    success: true,
+    orders,
+  });
 });
-
 
 // Get All Orders ---ADMIN
 exports.getAllOrders = asyncErrorHandler(async (req, res, next) => {
+  const orders = await Order.find();
 
-    const orders = await Order.find();
+  if (!orders) {
+    return next(new ErrorHandler("Order Not Found", 404));
+  }
 
-    if (!orders) {
-        return next(new ErrorHandler("Order Not Found", 404));
-    }
+  let totalAmount = 0;
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
 
-    let totalAmount = 0;
-    orders.forEach((order) => {
-        totalAmount += order.totalPrice;
-    });
-
-    res.status(200).json({
-        success: true,
-        orders,
-        totalAmount,
-    });
+  res.status(200).json({
+    success: true,
+    orders,
+    totalAmount,
+  });
 });
 
 // Update Order Status ---ADMIN
 exports.updateOrder = asyncErrorHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
 
-    const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(new ErrorHandler("Order Not Found", 404));
+  }
 
-    if (!order) {
-        return next(new ErrorHandler("Order Not Found", 404));
-    }
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHandler("Already Delivered", 400));
+  }
 
-    if (order.orderStatus === "Delivered") {
-        return next(new ErrorHandler("Already Delivered", 400));
-    }
-
-    if (req.body.status === "Shipped") {
-        order.shippedAt = Date.now();
-        order.orderItems.forEach(async (i) => {
-            await updateStock(i.product, i.quantity)
-        });
-    }
-
-    order.orderStatus = req.body.status;
-    if (req.body.status === "Delivered") {
-        order.deliveredAt = Date.now();
-    }
-
-    await order.save({ validateBeforeSave: false });
-
-    res.status(200).json({
-        success: true
+  if (req.body.status === "Shipped") {
+    order.shippedAt = Date.now();
+    order.orderItems.forEach(async (i) => {
+      await updateStock(i.product, i.quantity);
     });
+  }
+
+  order.orderStatus = req.body.status;
+  if (req.body.status === "Delivered") {
+    order.deliveredAt = Date.now();
+  }
+
+  await order.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+  });
 });
 
 async function updateStock(id, quantity) {
-    const product = await Product.findById(id);
-    product.stock -= quantity;
-    await product.save({ validateBeforeSave: false });
+  const product = await Product.findById(id);
+  product.stock -= quantity;
+  await product.save({ validateBeforeSave: false });
 }
 
 // Delete Order ---ADMIN
 exports.deleteOrder = asyncErrorHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
 
-    const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(new ErrorHandler("Order Not Found", 404));
+  }
 
-    if (!order) {
-        return next(new ErrorHandler("Order Not Found", 404));
-    }
+  await order.remove();
 
-    await order.remove();
-
-    res.status(200).json({
-        success: true,
-    });
+  res.status(200).json({
+    success: true,
+  });
 });
