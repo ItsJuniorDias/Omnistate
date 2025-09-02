4fbaf410e4265523c8295d1b9d8975a688f9589d
Alexandre <reabilitado97@gmail.com>
2025-09-02 19:37:51 -0300

    chore: adjust files

--- front-ent/src/datas/faqs/accounting.js
+++
@@ -1,8 +0,0 @@
-const legalQ = [
-  {
-    question: "What accounting evidence will I receive as an investor?",
-    answer: "We will provide the income assignment contract, which can be downloaded from the property sheet."
-  }
-]
-
-export default legalQ;
--- front-ent/webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // ...outras configs
-  resolve: {
-    fallback: {
-      crypto: require.resolve("crypto-browserify"),
-    },
-  },
-};
